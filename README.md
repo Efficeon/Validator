538d0a8055e31eb040f12ed486331a8ecd5ca48c
Leonid Dubravsky <efficeon2011@gmail.com>
2017-08-04 16:17:35 +0300

    add swager

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## Developed by Leonid Dubravsky
 
 URL for use validator http://localhost:8089/validate/
+
+URL for use swager    http://localhost:8089/swagger-ui.html
---
+++ src/main/java/com/validator/AppConfig.java
@@ -0,0 +1,18 @@
+package com.validator;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by lion on 04.08.17.
+ */
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
