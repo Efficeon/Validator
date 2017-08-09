b2c94e8256df67d5c3820adf5ffa162364dc86e6
Leonid Dubravsky <efficeon2011@gmail.com>
2017-08-09 11:04:04 +0300

    update swager

--- src/main/java/com/validator/AppConfig.java
+++ src/main/java/com/validator/AppConfig.java
@@ -9,10 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("swagger-ui.html");
     }
 }
